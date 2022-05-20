74ce393844c5a118b6caa1c75dcfbbd08cd11bae
Ismael <ismael@Macbook2014s-MacBook-Pro.local>
2022-05-20 08:44:37 +0300

    fix build

--- pages/index.js
+++ pages/index.js
@@ -15,27 +15,27 @@ export default function Home() {
       <p className="font-serif text-lg">Qasid’s Student Information System</p>
       <figure className="bg-slate-100 rounded-xl p-8 dark:bg-slate-800 max-w-md mt-5">
         <img
-          class="w-24 h-24 rounded-full mx-auto"
+          className="w-24 h-24 rounded-full mx-auto"
           src={data.results[0].picture.medium}
           alt=""
           width="384"
           height="512"
         />
-        <div class="pt-6 text-center space-y-4">
+        <div className="pt-6 text-center space-y-4">
           <blockquote>
-            <p class="text-lg font-medium">
+            <p className="text-lg font-medium">
               {data.results[0].location.street.number}{" "}
               {data.results[0].location.street.name},{" "}
               {data.results[0].location.city},{" "}
               {data.results[0].location.country}
             </p>
           </blockquote>
-          <figcaption class="font-medium">
-            <div class="text-sky-500 dark:text-sky-400">
+          <figcaption className="font-medium">
+            <div className="text-sky-500 dark:text-sky-400">
               {data.results[0].name.title} {data.results[0].name.first}{" "}
               {data.results[0].name.last}
             </div>
-            <div class="text-slate-700 dark:text-slate-500">
+            <div className="text-slate-700 dark:text-slate-500">
               {data.results[0].dob.age}, {data.results[0].gender}
             </div>
           </figcaption>
