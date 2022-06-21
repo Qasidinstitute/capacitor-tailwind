4b63758c3574aadf8263b1d2e26699b91ad83b9c
Noora <noora.chahine@gmail.com>
2022-06-21 17:37:43 +0300

    debugging graphql response

--- components/Users.js
+++ components/Users.js
@@ -5,30 +5,25 @@ import RealmContext from '../lib/RealmContext';
 import { generateAuthHeader } from '../lib/RealmClient';
 import { isAnon } from "../utils/anon";
 
-const fetcher = async ( query ) =>
-  fetch( process.env.NEXT_PUBLIC_REALM_GRAPHQL, {
-    method: 'POST',
-    headers: {
-      'Content-type': 'application/json',
-      'Authorization': await generateAuthHeader().then( data => { return data.Authorization } ),
-    },
-    body: JSON.stringify({ query })
-  }).then( data => { data.json() } ).catch( e => { console.log( e ) } );
+const FIND_GRADES = `query {
+  programs(query: { sort: _id }) {
+  _id
+  }
+}`
 
+const fetcher = async () => fetch({
+  method: "POST",
+  headers: {
+    //contentType: 'application/json',
+    apiKey: "KX7quS1GK13d8M6TXFUihbIBK9kntDcvlQmKsglvoeLMVdQs9ZKAo8DOlfYC2ovZ"
+  },
+  body: { query: JSON.stringify(FIND_GRADES) },
+}).then( (res) => JSON.stringify(res) ).catch( (e) => console.log(JSON.stringify(e)) );
 
-  const FIND_GRADES = `query {
-    programs(query: { sort: _id }) {
-    _id
-    }
-  }`
+const findPrograms = async () => {
+  const { data, error } = useSWR( process.env.NEXT_PUBLIC_REALM_GRAPHQL, fetcher );
 
-function findPrograms() {
-
-  const { data, error } = useSWR(FIND_GRADES, fetcher);
-
-  const token = async () => generateAuthHeader().then( res => { console.log( res ) } )
-
-  console.log(token)
+  console.log(data)
 
   return {
     programs: data,
@@ -41,6 +36,9 @@ function Users ({realmContext: {app, user, setUser}}) {
   const loggedIn = !isAnon( user );
 
   const { programs, isLoading, isError } = findPrograms();
+
+  //console.log(findPrograms())
+  //console.log(programs)
 
   return (
     <div>
